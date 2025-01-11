2c04e7cc95b9f7339646863cfe71938cd349a19f
bhavin2004 <94212977+bhavin2004@users.noreply.github.com>
2025-01-11 04:30:32 +0000

    project structure addded

---
+++ template.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s]: %(message)s:'
+)
+
+
+project_name = "Chicken-Disease-Classifier"
+
+list_of_files = [
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    # f'src/{project_name}/components/',
+    f'src/{project_name}/pipelines/__init__.py',
+    f'src/{project_name}/pipelines/__init__.py',
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/constants/__init__.py',
+    'config/config.yaml',
+    'dvc.yaml',
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    'app.py'
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    file_dir,file_name=os.path.split(filepath)
+    
+    if file_dir!="":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f"Creating Directory: {file_dir} for the file:{file_name}")
+    
+    if (not os.path.exists(filepath)):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file:{filepath}")
+        
+    else:
+        logging.info(f'{file_name} is already exist')
+        
+        
